d6fc18c79b03b56f45c2189697679c1e8bf9eeec
Cliff Rodgers + Pearl Latteier <dev+kliph+pearlbea@bendyworks.com>
2016-05-06 11:01:03 -0500

    browserify task works; generating a datum

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt){
+
+    'use strict';
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch : {
+            browserify : {
+                files : ['public/js/**/*.js'],
+                tasks : ['browserify']
+            }
+        },
+
+        browserify : {
+            app : {
+                src: ['public/js/*.js'],
+                dest: 'public/dist/main.js',
+                options: {
+                    watch: true,
+                    transform: [ ["babelify", { presets: ["es2015"] }] ]
+                }
+            }
+        },
+
+        clean : {
+            dist : ['public/dist']
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+
+    grunt.registerTask('default',['clean', 'browserify']);
+
+
+
+};
---
+++ public/js/data.js
@@ -0,0 +1,30 @@
+// date, time, person, project, value, category [joy, success]
+import { sample, range } from 'lodash';
+import "babel-polyfill";
+
+
+// {
+//   "timestamp": new Date(),
+//   "person_id": (sample [1, 2, 3, 4, 5, 6]),
+//   "project_id": 1,
+//   "value": (rand-int 5),
+//   "category_id": (rand-int 2)  // 1: joy, 2: success
+// }
+
+
+export function* generateDatum() {
+    var datum = {
+      "timestamp": function(){
+        var hour = sample(range(9, 17));
+        return new Date(2016, 3, 4, hour);
+      }(),
+      "person_id": sample([1, 2, 3, 4, 5, 6]),
+      "project_id": 1,
+      "value": sample([1, 2, 3, 4, 5]),
+      "category_id": sample([1, 2])
+    };
+    yield datum;
+  };
+
+var gen = generateDatum();
+console.log(gen.next().value);
